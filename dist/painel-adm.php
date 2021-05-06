41423632e5a15453032bae1504e65f9f7ca8ff3f
fabioportovasques <fabioportovasques321@gmail.com>
2021-05-06 17:49:48 -0500

    alteração na cor do dashboard

--- dist/painel-adm.php
+++ dist/painel-adm.php
@@ -168,15 +168,49 @@
 
                                 </nav>
                            </div>
+                        
+                     </nav>
+      
+                </div>
+                <br />
+              
 
-                         
+<!--CONTEUDO DO MEIO -->
+  <div class="container">
 
+                <br />
+                <br />
+                <br />
+                <br />
+                 <br />
+                <br />
+                <br />
+                <br />
+    <div class="row">
+      <div class="col-sm-12">
+          <ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+</div>
+
+
+</div>
+</div>
+</div>
                             
-                    
-     
-
-
-
+ 
 
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
