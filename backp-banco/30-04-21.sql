d405bd08caf5b0b2e55efb1d3f2fdcaa3e9d7ec6
fabioportovasques <fabioportovasques321@gmail.com>
2021-05-17 22:25:38 -0500

    alteração modelo logico

--- backp-banco/30-04-21.sql
+++ backp-banco/30-04-21.sql
@@ -1,7 +1,45 @@
-SELECT * FROM especialidade;
-SELECT * FROM agenda;
-SELECT * FROM usuarios;
-select * from cliente;
+
+SELECT * FROM `medico` ;
+
+SELECT * FROM especialidade ;
+
+SELECT * 
+  FROM medico m
+       INNER JOIN 
+       especialidade_has_medico espec  
+       ON 
+       espec.especialidade_cod-especialidade = espec.medico_cod-medico;
+       
+
+SELECT especialidade_cod-especialidade AS especialidade FROM `agenda-ubs`  ;
+
+SELECT 
+* 
+FROM especialidade espec 
+inner join medico m 
+on
+espec.cod-especialidade = m.cod-medico
+inner join especialidade_has_medico e
+on
+e.medico_cod-medico = m.cod-medico
+inner join especialidade_has_medico e 
+on espec.cod-especialidade = e.especialidade_cod-especialidade; 
+
+/*
+testes
+
+*/
 
 
 
+SELECT 
+* 
+FROM especialidade espec 
+inner join medico m 
+on
+especialidade.cod-especialidade  = m.cod-medico
+inner join especialidade_has_medico e
+on
+e.medico_cod-medico = m.cod-medico
+inner join especialidade_has_medico es 
+on especialidade.cod-especialidade = e.especialidade_cod-especialidade; 
